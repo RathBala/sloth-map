e85b0b32bccfe1b84dd636774392ebdf687995d4
Rath Bala <rath.balas1@gmail.com>
2024-05-13 07:41:54 +0100

    WIP authN. Using PedroTech video at 9 minutes 50 seconds.

---
+++ src/components/Register.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+
+function Register() {
+    return (
+        <div className="register">
+            <div>
+                <h3>Create an account</h3>
+                <input placeholder="Email" />
+                <input placeholder="Password" />
+
+                <button>Create</button>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBg9oTSxgiXb9P_FPet0snym1R3szJawx8',
+    authDomain: 'budgie-dc9f6.firebaseapp.com',
+    projectId: 'budgie-dc9f6',
+    storageBucket: 'budgie-dc9f6.appspot.com',
+    messagingSenderId: '105899101971',
+    appId: '1:105899101971:web:2c0cd86a5f2bc28c6a54e6',
+    measurementId: 'G-G2Q5FYSZN8',
+};
+
+const app = initializeApp(firebaseConfig);
