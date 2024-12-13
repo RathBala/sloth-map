11c536910b3614ff46dca161bcb7202b092cfc93
Rath Bala <rath.balas1@gmail.com>
2024-12-13 08:07:45 +0000

    WIP AuthContext. Drafted the rough structure. TODO - see how I might simplify useUserData based on this.

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,3 +1,21 @@
-import { createContext } from 'react';
+import { createContext, useEffect } from 'react';
 
-export const AuthContext = createContext(false);
+export const AuthContext = createContext({ currentUser: null });
+
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            setCurrentUser(user);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ currentUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
